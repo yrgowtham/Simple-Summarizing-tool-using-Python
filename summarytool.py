a542c346e462cff098476399b40cde84e8d755d5
yrgowtham <gowtham1@mail.usf.edu>
2016-02-25 19:13:10 -0500

    Create summarytool.py

---
+++ summarytool.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+import re
+
+
+class SummaryTool(object):
+
+    # Naive method for splitting a text into sentences
+    def split_content_to_sentences(self, content):
+        content = content.replace("\n", ". ")
+        return content.split(". ")
+
+    # Naive method for splitting a text into paragraphs
+    def split_content_to_paragraphs(self, content):
+        return content.split("\n\n")
+
+    # Caculate the intersection between 2 sentences
+    def sentences_intersection(self, sent1, sent2):
+
+        # split the sentence into words/tokens
+        s1 = set(sent1.split(" "))
+        s2 = set(sent2.split(" "))
+
+        # If there is not intersection, just return 0
+        if (len(s1) + len(s2)) == 0:
+            return 0
+
+        # We normalize the result by the average number of words
+        return len(s1.intersection(s2)) / ((len(s1) + len(s2)) / 2)
+
+    # Format a sentence - remove all non-alphbetic chars from the sentence
+    # We'll use the formatted sentence as a key in our sentences dictionary
+    def format_sentence(self, sentence):
+        sentence = re.sub(r'\W+', '', sentence)
+        return sentence
+
+    # Convert the content into a dictionary <K, V>
+    # k = The formatted sentence
+    # V = The rank of the sentence
+    def get_senteces_ranks(self, content):
+
+        # Split the content into sentences
+        sentences = self.split_content_to_sentences(content)
+
+        # Calculate the intersection of every two sentences
+        n = len(sentences)
+        values = [[0 for x in xrange(n)] for x in xrange(n)]
+        for i in range(0, n):
+            for j in range(0, n):
+                values[i][j] = self.sentences_intersection(sentences[i], sentences[j])
+
+        # Build the sentences dictionary
+        # The score of a sentences is the sum of all its intersection
+        sentences_dic = {}
+        for i in range(0, n):
+            score = 0
+            for j in range(0, n):
+                if i == j:
+                    continue
+                score += values[i][j]
+            sentences_dic[self.format_sentence(sentences[i])] = score
+        return sentences_dic
+
+    # Return the best sentence in a paragraph
+    def get_best_sentence(self, paragraph, sentences_dic):
+
+        # Split the paragraph into sentences
+        sentences = self.split_content_to_sentences(paragraph)
+
+        # Ignore short paragraphs
+        if len(sentences) < 2:
+            return ""
+
+        # Get the best sentence according to the sentences dictionary
+        best_sentence = ""
+        max_value = 0
+        for s in sentences:
+            strip_s = self.format_sentence(s)
+            if strip_s:
+                if sentences_dic[strip_s] > max_value:
+                    max_value = sentences_dic[strip_s]
+                    best_sentence = s
+
+        return best_sentence
+
+    # Build the summary
+    def get_summary(self, title, content, sentences_dic):
+
+        # Split the content into paragraphs
+        paragraphs = self.split_content_to_paragraphs(content)
+
+        # Add the title
+        summary = []
+        summary.append(title.strip())
+        summary.append("")
+
+        # Add the best sentence from each paragraph
+        for p in paragraphs:
+            sentence = self.get_best_sentence(p, sentences_dic).strip()
+            if sentence:
+                summary.append(sentence)
+
+        return ("\n").join(summary)
+
+
+# Main method, just run "python summary_tool.py"
+def main():
+
+    # Demo
+    # Content from: "http://thenextweb.com/apps/2013/03/21/swayy-discover-curate-content/"
+
+    title = """
+    Swayy is a beautiful new dashboard for discovering and curating online content [Invites]
+    """
+
+    content = """
+    Business Data Analyst, USF Health, Tampa, Florida Jun 2015 – Dec 2015 Areas: Data Analysis, SAS, SQL, Agile, Scrum, JIRA, Confluence, Quality Assurance .Worked directly with researchers and surgeons to understand requirements and help to build predictive models .Performed data analysis activities such as data profiling, data cleansing, creating data dictionaries and data exploration to identify trends, patterns and anomalies using SAS/SQL and Tableau; Create SAS/MACROS to standardize the workflow .Develop data-driven solutions to help facilitate decisions and actions to improve patients health and surgical processes . Worked in Agile/Scrum environment, with extensive use of tools such as JIRA and Confluence .Participated in the JAD Sessions to interact with stakeholders to communicate their perspective and reach agreements .Documented User Stories and facilitate Story Point discussions to analyze the level of effort on project specifications.
+    """
+
+    # Create a SummaryTool object
+    st = SummaryTool()
+
+    # Build the sentences dictionary
+    sentences_dic = st.get_senteces_ranks(content)
+
+    # Build the summary with the sentences dictionary
+    summary = st.get_summary(title, content, sentences_dic)
+
+    # Print the summary
+    print summary
+
+    # Print the ratio between the summary length and the original length
+    print ""
+    print "Original Length %s" % (len(title) + len(content))
+    print "Summary Length %s" % len(summary)
+    print "Summary Ratio: %s" % (100 - (100 * (len(summary) / (len(title) + len(content)))))
+
+
+if __name__ == '__main__':
+    main()
